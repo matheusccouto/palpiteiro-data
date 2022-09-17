a7e339c76f6696526c646dee6895edc55169a5c9
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-09-17 13:32:09 +0000

    Fix stg_partidas_match

--- models/staging/cartola/stg_partidas_match.sql
+++ models/staging/cartola/stg_partidas_match.sql
@@ -1,37 +1,38 @@
 SELECT
-    temporada AS season,
-    rodada AS round,
-    local AS venue,
-    valida AS valid,
-    c_home.slug AS home,
-    c_away.slug AS away,
-    clube_casa_posicao AS position_home,
-    clube_visitante_posicao AS position_away,
-    placar_oficial_mandante AS score_home,
-    placar_oficial_visitante AS score_away,
-    status_transmissao_tr AS broadcasting_status,
-    status_cronometro_tr AS broadcasting_time_status,
-    periodo_tr AS broadcasting_phase,
-    transmissao_label AS broadcasting_label,
-    transmissao_url AS broadcasting_url,
-    aproveitamento_mandante_4 AS previous_result_1_home,
-    aproveitamento_mandante_3 AS previous_result_2_home,
-    aproveitamento_mandante_2 AS previous_result_3_home,
-    aproveitamento_mandante_1 AS previous_result_4_home,
-    aproveitamento_mandante_0 AS previous_result_5_home,
-    aproveitamento_visitante_4 AS previous_result_1_away,
-    aproveitamento_visitante_3 AS previous_result_2_away,
-    aproveitamento_visitante_2 AS previous_result_3_away,
-    aproveitamento_visitante_1 AS previous_result_4_away,
-    aproveitamento_visitante_0 AS previous_result_5_away,
+    ptd.temporada AS season,
+    ptd.rodada AS round,
+    ptd.local AS venue,
+    ptd.valida AS valid,
+    ptd.c_home.slug AS home,
+    ptd.c_away.slug AS away,
+    ptd.clube_casa_posicao AS position_home,
+    ptd.clube_visitante_posicao AS position_away,
+    ptd.placar_oficial_mandante AS score_home,
+    ptd.placar_oficial_visitante AS score_away,
+    ptd.status_transmissao_tr AS broadcasting_status,
+    ptd.status_cronometro_tr AS broadcasting_time_status,
+    ptd.periodo_tr AS broadcasting_phase,
+    ptd.transmissao_label AS broadcasting_label,
+    ptd.transmissao_url AS broadcasting_url,
+    ptd.aproveitamento_mandante_4 AS previous_result_1_home,
+    ptd.aproveitamento_mandante_3 AS previous_result_2_home,
+    ptd.aproveitamento_mandante_2 AS previous_result_3_home,
+    ptd.aproveitamento_mandante_1 AS previous_result_4_home,
+    ptd.aproveitamento_mandante_0 AS previous_result_5_home,
+    ptd.aproveitamento_visitante_4 AS previous_result_1_away,
+    ptd.aproveitamento_visitante_3 AS previous_result_2_away,
+    ptd.aproveitamento_visitante_2 AS previous_result_3_away,
+    ptd.aproveitamento_visitante_1 AS previous_result_4_away,
+    ptd.aproveitamento_visitante_0 AS previous_result_5_away,
     CAST(
         (
-            temporada - 2000
-        ) * 100000000 + clube_casa_id * 10000 + clube_visitante_id AS INT
-    ) AS id,
-    TIMESTAMP(partida_data, "America/Sao_Paulo") AS timestamp,
-    TIMESTAMP(inicio_cronometro_tr) AS broadcasting_time_start
+            ptd.temporada - 2000
+        ) * 100000000 + ptd.clube_casa_id
+        * 10000 + ptd.clube_visitante_id AS INT
+    ) AS match_id,
+    TIMESTAMP(ptd.partida_data, "America/Sao_Paulo") AS timestamp, -- noqa: L029
+    TIMESTAMP(ptd.inicio_cronometro_tr) AS broadcasting_time_start
 FROM
-    {{ source ('cartola', 'partidas') }}
-LEFT JOIN {{ ref ("dim_club") }} AS c_home ON clube_casa_id = c_home.id
-LEFT JOIN {{ ref ("dim_club") }} AS c_away ON clube_visitante_id = c_away.id
+    {{ source ('cartola', 'partidas') }} AS ptd
+LEFT JOIN {{ ref ("dim_club") }} AS c_home ON ptd.clube_casa_id = c_home.id
+LEFT JOIN {{ ref ("dim_club") }} AS c_away ON ptd.clube_visitante_id = c_away.id
