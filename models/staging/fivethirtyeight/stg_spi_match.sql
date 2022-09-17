2e84a38ccec57cbf15191685bbbe9c5f920ae4cd
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-09-17 13:56:44 +0000

    fix fivethirtyeight staging

--- models/staging/fivethirtyeight/stg_spi_match.sql
+++ models/staging/fivethirtyeight/stg_spi_match.sql
@@ -1,29 +1,31 @@
 SELECT
-    CONCAT(CAST(season AS STRING), " ", team1, " x ", team2) AS id,
-    DATE(date) AS date,
-    season AS season,
-    league_id AS league_id,
-    league AS league,
-    c_home.slug AS home,
-    c_away.slug AS away,
-    spi1 AS spi_home,
-    spi2 AS spi_away,
-    prob1 AS prob_home,
-    prob2 AS prob_away,
-    probtie AS prob_tie,
-    importance1 AS importance_home,
-    importance2 AS importance_away,
-    proj_score1 AS proj_score_home,
-    proj_score2 AS proj_score_away,
-    score1 AS score_home,
-    score2 AS score_away,
-    adj_score1 AS adj_score_home,
-    adj_score2 AS adj_score_away,
-    xg1 AS xg_home,
-    xg2 AS xg_away,
-    nsxg1 AS nsxg_home,
-    nsxg2 AS nsxg_away
+    spi.season AS season,
+    spi.league_id AS league_id,
+    spi.league AS league,
+    spi.c_home.slug AS home,
+    spi.c_away.slug AS away,
+    spi.spi1 AS spi_home,
+    spi.spi2 AS spi_away,
+    spi.prob1 AS prob_home,
+    spi.prob2 AS prob_away,
+    spi.probtie AS prob_tie,
+    spi.importance1 AS importance_home,
+    spi.importance2 AS importance_away,
+    spi.proj_score1 AS proj_score_home,
+    spi.proj_score2 AS proj_score_away,
+    spi.score1 AS score_home,
+    spi.score2 AS score_away,
+    spi.adj_score1 AS adj_score_home,
+    spi.adj_score2 AS adj_score_away,
+    spi.xg1 AS xg_home,
+    spi.xg2 AS xg_away,
+    spi.nsxg1 AS nsxg_home,
+    spi.nsxg2 AS nsxg_away,
+    CONCAT(
+        CAST(spi.season AS STRING), " ", spi.team1, " x ", spi.team2
+    ) AS spi_id,
+    DATE(spi.date) AS date -- noqa: L029
 FROM
-    {{ source ('fivethirtyeight', 'spi') }}
-LEFT JOIN {{ ref ("dim_slug") }} AS c_home ON team1 = c_home.name
-LEFT JOIN {{ ref ("dim_slug") }} AS c_away ON team2 = c_away.name
+    {{ source ('fivethirtyeight', 'spi') }} AS spi
+LEFT JOIN {{ ref ("dim_slug") }} AS c_home ON spi.team1 = c_home.name
+LEFT JOIN {{ ref ("dim_slug") }} AS c_away ON spi.team2 = c_away.name
