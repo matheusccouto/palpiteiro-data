a1cedaec28c87d8407aedb75646abd11007ff740
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-10-13 22:17:37 +0000

    Change the way draft_norm is calculated

--- models/mart/cartola/fct_popular.sql
+++ models/mart/cartola/fct_popular.sql
@@ -2,7 +2,7 @@ WITH agg AS (
     SELECT
         season,
         round,
-        SUM(drafts) AS drafts_sum
+        MAX(drafts) AS drafts_max
     FROM
         {{ ref("stg_destaques_popular") }}
     GROUP BY
@@ -14,7 +14,7 @@ SELECT
     p.round,
     p.player_id,
     p.drafts,
-    p.drafts / agg.drafts_sum AS drafts_norm
+    p.drafts / agg.drafts_max AS drafts_norm
 FROM
     {{ ref("stg_destaques_popular") }} AS p
 LEFT JOIN agg ON p.season = agg.season AND p.round = agg.round
