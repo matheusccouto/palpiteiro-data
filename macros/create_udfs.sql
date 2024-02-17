27c58f879e72bc282a8b0c4254abc6868c102505
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2024-02-17 22:28:24 +0000

    Update macros

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,7 +1,5 @@
 {% macro create_udfs() %}
 
 {% do run_query(predict_points()) %}
-{% do run_query(points_cartola()) %}
-{% do run_query(all_time_round()) %}
 
 {% endmacro %}
--- macros/predict_points.sql
+++ macros/predict_points.sql
@@ -2,8 +2,6 @@
 
 CREATE OR REPLACE FUNCTION {{ target.dataset }}.PREDICT_POINTS(player_json JSON)
 RETURNS FLOAT64
-REMOTE WITH CONNECTION `us-east4.remote-function` OPTIONS (
-    endpoint = 'https://us-east4-{{ target.project }}.cloudfunctions.net/points'
-)
+REMOTE WITH CONNECTION `us-east4.remote-function` OPTIONS (endpoint = 'https://us-east4-{{ target.project }}.cloudfunctions.net/points')
 
 {% endmacro %}
