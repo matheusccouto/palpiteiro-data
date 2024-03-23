d1959c289c80efe02b8857bf8c52b66061f8efd9
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2024-03-23 19:33:54 +0000

    Apply sqlfluff

--- models/mart/cartola/plays.sql
+++ models/mart/cartola/plays.sql
@@ -53,7 +53,7 @@ join_matches as (
     select
         play.*,
         coalesce(home.date, away.date) as date,
-        if(away.match_id is null, 'home', 'away') as venue,
+        if(away.match_id is NULL, 'home', 'away') as venue,
         coalesce(home.away_id, away.home_id) as opponent_id,
         coalesce(home.valid, away.valid) as valid
 
@@ -78,7 +78,7 @@ join_matches as (
 
 join_picks as (
 
-    select 
+    select
         play.*,
         picks.picks,
         picks.picks_ratio
@@ -103,10 +103,10 @@ club_matches as (
         any_value(venue) as venue,
         any_value(valid) as valid,
         any_value(opponent_id) as opponent_id
-    
+
     from
         join_matches
-    
+
     group by club_id, season, round
 
 ),
@@ -120,10 +120,10 @@ club_scoring as (
         round(sum(total_points), 1) as total_points,
         round(sum(offensive_points), 1) as offensive_points,
         round(sum(defensive_points), 1) as defensive_points
-    
+
     from
-        player_scoring as score
-    
+        player_scoring
+
     group by season, round, club_id
 
 ),
@@ -141,18 +141,20 @@ join_club_scoring as (
 
     from
         club_matches as match
-    
+
     left join
         club_scoring as club
-        on match.club_id = club.club_id
-        and match.season = club.season
-        and match.round = club.round
+        on
+            match.club_id = club.club_id
+            and match.season = club.season
+            and match.round = club.round
 
     left join
         club_scoring as opponent
-        on match.opponent_id = opponent.club_id
-        and match.season = opponent.season
-        and match.round = opponent.round
+        on
+            match.opponent_id = opponent.club_id
+            and match.season = opponent.season
+            and match.round = opponent.round
 ),
 
 club_rolling as (
@@ -161,18 +163,78 @@ club_rolling as (
         club_id,
         season,
         round,
-        AVG(club_total_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_total_points_last_5_at,
-        AVG(club_offensive_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_offensive_points_last_5_at,
-        AVG(club_defensive_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_defensive_points_last_5_at,
-        AVG(opponent_total_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_total_points_last_5_at,
-        AVG(opponent_offensive_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_offensive_points_last_5_at,
-        AVG(opponent_defensive_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_defensive_points_last_5_at,
-        AVG(opponent_total_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_allowed_total_points_last_5_at,
-        AVG(opponent_offensive_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_allowed_offensive_points_last_5_at,
-        AVG(opponent_defensive_points) OVER (PARTITION BY club_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS club_allowed_defensive_points_last_5_at,
-        AVG(club_total_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_allowed_total_points_last_5_at,
-        AVG(club_offensive_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_allowed_offensive_points_last_5_at,
-        AVG(club_defensive_points) OVER (PARTITION BY opponent_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS opponent_allowed_defensive_points_last_5_at
+        avg(club_total_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_total_points_last_5_at,
+        avg(club_offensive_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_offensive_points_last_5_at,
+        avg(club_defensive_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_defensive_points_last_5_at,
+        avg(opponent_total_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_total_points_last_5_at,
+        avg(opponent_offensive_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_offensive_points_last_5_at,
+        avg(opponent_defensive_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_defensive_points_last_5_at,
+        avg(opponent_total_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_allowed_total_points_last_5_at,
+        avg(opponent_offensive_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_allowed_offensive_points_last_5_at,
+        avg(opponent_defensive_points)
+            over (
+                partition by club_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as club_allowed_defensive_points_last_5_at,
+        avg(club_total_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_allowed_total_points_last_5_at,
+        avg(club_offensive_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_allowed_offensive_points_last_5_at,
+        avg(club_defensive_points)
+            over (
+                partition by opponent_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as opponent_allowed_defensive_points_last_5_at
 
     from
         join_club_scoring
@@ -183,36 +245,61 @@ player_rolling as (
 
     select
         *,
-        AVG(CAST(played AS INT)) OVER (PARTITION BY player_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS played_last_5_at,
-        AVG(total_points) OVER (PARTITION BY player_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS total_points_last_5_at,
-        AVG(offensive_points) OVER (PARTITION BY player_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS offensive_points_last_5_at,
-        AVG(defensive_points) OVER (PARTITION BY player_id, venue ORDER BY season, round ROWS BETWEEN 5 PRECEDING AND 1 PRECEDING) AS defensive_points_last_5_at,
+        avg(cast(played as INT))
+            over (
+                partition by player_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as played_last_5_at,
+        avg(total_points)
+            over (
+                partition by player_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as total_points_last_5_at,
+        avg(offensive_points)
+            over (
+                partition by player_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as offensive_points_last_5_at,
+        avg(defensive_points)
+            over (
+                partition by player_id, venue
+                order by season, round rows between 5 preceding and 1 preceding
+            )
+            as defensive_points_last_5_at
 
-    from join_picks as play
-      
+    from join_picks
+
 ),
 
 join_club_rolling as (
 
     select
         player.*,
-        club_total_points_last_5_at,
-        club_offensive_points_last_5_at,
-        club_defensive_points_last_5_at,
-        opponent_total_points_last_5_at,
-        opponent_offensive_points_last_5_at,
-        opponent_defensive_points_last_5_at,
-        club_allowed_total_points_last_5_at,
-        club_allowed_offensive_points_last_5_at,
-        club_allowed_defensive_points_last_5_at,
-        opponent_allowed_total_points_last_5_at,
-        opponent_allowed_offensive_points_last_5_at,
-        opponent_allowed_defensive_points_last_5_at
-    
+        club.club_total_points_last_5_at,
+        club.club_offensive_points_last_5_at,
+        club.club_defensive_points_last_5_at,
+        club.opponent_total_points_last_5_at,
+        club.opponent_offensive_points_last_5_at,
+        club.opponent_defensive_points_last_5_at,
+        club.club_allowed_total_points_last_5_at,
+        club.club_allowed_offensive_points_last_5_at,
+        club.club_allowed_defensive_points_last_5_at,
+        club.opponent_allowed_total_points_last_5_at,
+        club.opponent_allowed_offensive_points_last_5_at,
+        club.opponent_allowed_defensive_points_last_5_at
+
     from
         player_rolling as player
-    
-    left join club_rolling as club on player.club_id = club.club_id and player.season = club.season and player.round = club.round
+
+    left join
+        club_rolling as club
+        on
+            player.club_id = club.club_id
+            and player.season = club.season
+            and player.round = club.round
 
 )
 
